ea70df710649de951fd91e248e9ab44f49f2ef2c
romain <termitatur@msn.com>
2019-11-25 22:39:44 +0100

    add admin module

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminAdController extends AbstractController
 {
@@ -16,5 +23,88 @@ class AdminAdController extends AbstractController
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll(),
         ]);
+    }
+
+    /**
+     * Permet de modifier une annonce
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     */
+    public function edit (Ad $ad, Request $request, ObjectManager $manager){
+
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig',[
+            'form' => $form->createView(),
+            'ad' => $ad
+        ]);
+    }
+
+
+    /**
+     * Permet d'effacer une annonce
+     *
+     * @param Ad $ad
+     * @param ObjectManager $manager
+     * 
+     * @return void
+    * @Route("/admin/ads/{id}/delete", name="admin_ads_delete")
+     */
+    public function delete(Ad $ad, ObjectManager $manager){
+
+        $manager->remove($ad);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée"
+        );
+
+        return $this->redirectToRoute('admin_ads_index');
+    }
+
+    /**
+     * @Route("/admin/comments", name="admin_comments_index")
+     */
+    public function comment(CommentRepository $repo)
+    {
+        return $this->render('admin/comment/index.html.twig', [
+            'COMMENTS' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * Permet d'afficher et de gerer le formulaire de connexion
+     * @Route("/admin/login", name="admin_account_login")
+     * 
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+        return $this->render('admin/account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * Permet la déconnexion de l'utilisateur admin
+     * 
+     * @Route("/admin/logout", name="admin_account_logout")
+     */
+    public function logout(){
+
     }
 }
